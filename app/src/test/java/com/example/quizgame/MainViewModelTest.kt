d4a90adce0102382135fa25bcb7e3b3f70c67ad5
Anton Gorokhov <bonduelle85@gmail.com>
2024-04-23 20:48:16 +0300

    MainViewModelTest, FakeRepository

--- app/src/test/java/com/example/quizgame/MainViewModelTest.kt
+++ app/src/test/java/com/example/quizgame/MainViewModelTest.kt
@@ -19,13 +19,13 @@ class MainViewModelTest {
 
     @Test
     fun caseNumberOne() {
-        var actualUiState : UiState = viewModel.init()
-        var expectedUiState : UiState = UiState.Question(
+        var actualUiState: UiState = viewModel.init()
+        var expectedUiState: UiState = UiState.Question(
             questionUiState = QuestionUiState.Base(value = "question number 1"),
             choiceOneUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 1"),
-            choiceTwoUiState = ChoiceUiState.AvailableToChoose(value = "q2 choice 2"),
-            choiceThreeUiState = ChoiceUiState.AvailableToChoose(value = "q3 choice 3"),
-            choiceFourUiState = ChoiceUiState.AvailableToChoose(value = "q4 choice 4"),
+            choiceTwoUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 2"),
+            choiceThreeUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 3"),
+            choiceFourUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 4"),
             actionUiState = ActionUiState.None
         )
         assertEquals(expectedUiState, actualUiState)
@@ -64,13 +64,13 @@ class MainViewModelTest {
 
     @Test
     fun caseNumberTwo() {
-        var actualUiState : UiState = viewModel.init()
-        var expectedUiState : UiState = UiState.Question(
+        var actualUiState: UiState = viewModel.init()
+        var expectedUiState: UiState = UiState.Question(
             questionUiState = QuestionUiState.Base(value = "question number 1"),
-            choiceOneUiState = ChoiceUiState.AvailableToChoose(value = "choice 1"),
-            choiceTwoUiState = ChoiceUiState.AvailableToChoose(value = "choice 2"),
-            choiceThreeUiState = ChoiceUiState.AvailableToChoose(value = "choice 3"),
-            choiceFourUiState = ChoiceUiState.AvailableToChoose(value = "choice 4"),
+            choiceOneUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 1"),
+            choiceTwoUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 2"),
+            choiceThreeUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 3"),
+            choiceFourUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 4"),
             actionUiState = ActionUiState.None
         )
         assertEquals(expectedUiState, actualUiState)
@@ -109,13 +109,13 @@ class MainViewModelTest {
 
     @Test
     fun caseNumberThree() {
-        var actualUiState : UiState = viewModel.init()
-        var expectedUiState : UiState = UiState.Question(
+        var actualUiState: UiState = viewModel.init()
+        var expectedUiState: UiState = UiState.Question(
             questionUiState = QuestionUiState.Base(value = "question number 1"),
-            choiceOneUiState = ChoiceUiState.AvailableToChoose(value = "choice 1"),
-            choiceTwoUiState = ChoiceUiState.AvailableToChoose(value = "choice 2"),
-            choiceThreeUiState = ChoiceUiState.AvailableToChoose(value = "choice 3"),
-            choiceFourUiState = ChoiceUiState.AvailableToChoose(value = "choice 4"),
+            choiceOneUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 1"),
+            choiceTwoUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 2"),
+            choiceThreeUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 3"),
+            choiceFourUiState = ChoiceUiState.AvailableToChoose(value = "q1 choice 4"),
             actionUiState = ActionUiState.None
         )
         assertEquals(expectedUiState, actualUiState)
@@ -159,5 +159,54 @@ class MainViewModelTest {
             actionUiState = ActionUiState.Check
         )
         assertEquals(expectedUiState, actualUiState)
+    }
+}
+
+private class FakeRepository : Repository {
+
+    private val questionAndChoicesList = listOf(
+        QuestionAndChoices(
+            question = "question number 1",
+            choiceOne = "q1 choice 1",
+            choiceTwo = "q1 choice 2",
+            choiceThree = "q1 choice 3",
+            choiceFour = "q1 choice 4",
+        ),
+        QuestionAndChoices(
+            question = "question number 2",
+            choiceOne = "q2 choice 1",
+            choiceTwo = "q2 choice 2",
+            choiceThree = "q2 choice 3",
+            choiceFour = "q2 choice 4",
+        ),
+    )
+
+    private var currentIndex = 0
+    private var userChoiceIndex = -1
+
+    override fun questionAndChoices(): QuestionAndChoices {
+        return questionAndChoicesList[currentIndex]
+    }
+    override fun chooseFirst() {
+        userChoiceIndex = 0
+    }
+    override fun chooseSecond() {
+        userChoiceIndex = 1
+    }
+    override fun chooseThird() {
+        userChoiceIndex = 2
+    }
+    override fun chooseFourth() {
+        userChoiceIndex = 3
+    }
+    override fun check(): CheckResult {
+        return if (userChoiceIndex == 0)
+            CheckResult.Correct(correctIndex = 0)
+        else
+            CheckResult.Incorrect(correctIndex = 0, incorrectIndex = userChoiceIndex)
+    }
+    override fun next() {
+        currentIndex++
+        userChoiceIndex = -1
     }
 }
