5730b68e0f78a39618e76c6189a00b8d7141f1f5
Anton Gorokhov <bonduelle85@gmail.com>
2024-04-23 19:54:18 +0300

    MainActivity, viewBinding

--- app/src/main/java/com/example/quizgame/MainActivity.kt
+++ app/src/main/java/com/example/quizgame/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.quizgame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.quizgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel = MainViewModel()
+
+        binding.choiceOneButton.setOnClickListener {
+            val uiState: UiState = viewModel.chooseFirst()
+            uiState.update(binding)
+        }
+
+        binding.choiceTwoButton.setOnClickListener {
+            val uiState: UiState = viewModel.chooseSecond()
+            uiState.update(binding)
+        }
+
+        binding.choiceThreeButton.setOnClickListener {
+            val uiState: UiState = viewModel.chooseThird()
+            uiState.update(binding)
+        }
+
+        binding.choiceFourButton.setOnClickListener {
+            val uiState: UiState = viewModel.chooseFourth()
+            uiState.update(binding)
+        }
+
+        binding.actionButton.setOnClickListener {
+            val uiState: UiState = viewModel.handleAction()
+            uiState.update(binding)
+        }
+
+        val uiState: UiState = viewModel.init()
+        uiState.update(binding)
     }
 }
